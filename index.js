50fb90b1b8ac27507f5133513a76469f4c6b6f39
jakechaley <jakechaley@gmail.com>
2024-01-12 20:49:21 -0800

    now tell users who's turn it currently is. Only injecting x or o in one box currently... working to figure out

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+let playerOneTurn = false;
+
+console.log("Default value of bool is" ,playerOneTurn)
+
+function toggle() {
+  playerOneTurn = !playerOneTurn;
+
+  console.log("It's Player one's turn!", playerOneTurn);
+}
+
 function tdClicked(event){
   const click = "This thing on?"
   console.log(click)
-  document.getElementById("markX").innerHTML = "X"
+  if(playerOneTurn === true) {
+    document.getElementById("markX").innerHTML = "X"
+    document.getElementById("playerIndicator").innerHTML = "Player 1, It's you're turn!"
+    toggle()
+  } else {
+    document.getElementById("markX").innerHTML = "O"
+    document.getElementById("playerIndicator").innerHTML = "Player 2, It's you're turn!"
+    toggle()
+  }
   event.stopPropagation()
 };
