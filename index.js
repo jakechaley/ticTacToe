0afa713427540bce46e48570d13e9ceb90c6ccba
jakechaley <jakechaley@gmail.com>
2024-01-15 17:52:19 -0800

    Start game behavior working closer to expectation

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ console.log("Default value of bool is" ,playerOneTurn)
 
 function toggle() {
   playerOneTurn = !playerOneTurn;
-  
-
-  console.log("It's Player one's turn!", playerOneTurn);
 }
+
+function startGame() {
+  document.getElementById("playerIndicator").innerHTML = "Player 1, It's you're turn!";
+
+  toggle()
+}
+
+
 
 function tdClicked(event){
   const click = "This thing on?"
   console.log(click)
   if(playerOneTurn === true) {
     document.getElementById("markX").innerHTML = "X"
-    document.getElementById("playerIndicator").innerHTML = "Player 1, It's you're turn!"
+    document.getElementById("playerIndicator").innerHTML = "Player 2, It's you're turn!"
     toggle()
   } else {
     document.getElementById("markX").innerHTML = "O"
-    document.getElementById("playerIndicator").innerHTML = "Player 2, It's you're turn!"
+    document.getElementById("playerIndicator").innerHTML = "Player 1, It's you're turn!"
     toggle()
   }
   event.stopPropagation()
